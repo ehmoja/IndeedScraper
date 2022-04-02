f314e22425f83fe0b03a489a2d3213ca8398b738
Ehsan Mojahedi <eh.mojahedi@gmail.com>
2022-04-02 09:13:08 +0430

    Removed unused header

--- scraper.py
+++ scraper.py
@@ -6,7 +6,6 @@ import time
 
 
 def extract(url, page):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36'}
     url = url+str(page)
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
